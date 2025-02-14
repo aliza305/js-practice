12203d0964dbafd939728c6d08ce4c575da0abc3
aliza305 <alizakashif2809@gmail.com>
2025-02-14 12:19:38 +0000

    conversions of datatype

---
+++ basics.js/conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "44"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN Not a number
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true , 0 => false
+//"" => false
