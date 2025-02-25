e6a68efca5cfc2e867f3ee348fd5d59e93ab9f0d
aliza305 <alizakashif2809@gmail.com>
2025-02-25 10:16:24 +0000

    operations

---
+++ basics.js/operations.js
@@ -0,0 +1,13 @@
+let number = 3
+let negnumber = -value
+console.log(negnumber);
+
+let str1 = "Hello"
+let str2 = "Aliza"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let gameCounter = 1000
+gameCounter++;
+console.log(gameCounter);
