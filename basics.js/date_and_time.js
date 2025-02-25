8d41bb71fd7141806ba15c14bee2337b76816d23
aliza305 <alizakashif2809@gmail.com>
2025-02-25 14:55:59 +0000

    date in js

---
+++ basics.js/date_and_time.js
@@ -0,0 +1,21 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+
+
+
+let myCreatedDate = new Date(2025,0,28)
+console.log(myCreatedDate.toDateString);
+
+let myTimeStamp = Date.now
+
+console.log(myCreatedDate.getTime());
+console.log(myTimeStamp);
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
