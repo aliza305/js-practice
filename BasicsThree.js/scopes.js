99f7783a4783895f178d5976d5fd74f2f19b448c
aliza305 <alizakashif2809@gmail.com>
2025-02-28 12:01:27 +0000

    scopes.js

---
+++ BasicsThree.js/scopes.js
@@ -0,0 +1,39 @@
+function one(){
+    const username = "Aliza"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "Aliza"
+    if(username === "Aliza"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+
+console.log(username);
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+const addtwo = function(num){
+    return num + 2 
+
+}
+
+addtwo(5)
