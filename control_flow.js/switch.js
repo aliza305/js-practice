df4ce4a89e540a536d9bf357504613082130003b
aliza305 <alizakashif2809@gmail.com>
2025-03-14 10:47:03 +0000

    switch statement

---
+++ control_flow.js/switch.js
@@ -0,0 +1,32 @@
+/*switch (key) {
+    case value:
+        
+        break;               Basic syntax of switch statement
+
+    default:
+        break;
+}*/
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Februray");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
